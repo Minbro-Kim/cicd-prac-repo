6ca8dab93b4541c44f8c66e49368586b7375849b
Minbro-Kim <sojangjangi@gmail.com>
2026-03-02 22:48:05 +0900

    chore: setup Jacoco and CI/CT quality gate test

--- src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
+++ src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
@@ -1,5 +1,8 @@
 package com.sprint.mission.discodeit;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DiscodeitApplicationTests {
 
 	@Test
+	@DisplayName("Application Context Loading Test")
 	void contextLoads() {
+		assertDoesNotThrow(()->{
+
+		}, "No exception thrown");
 	}
 
 }
